945f566961d035d530c60b6b0625e6d8cfb6386a
viktorhugodev <viktorhugo.dev@gmail.com>
2023-02-24 09:26:59 -0300

    add pdf save with puppet

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,29 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
 import { document } from '../utils/dynamoDbClient'
+import {compile} from 'handlebars'
+import { join } from 'path'
+import { readFileSync } from 'fs'
+import chromium from 'chrome-aws-lambda'
+import dayjs from 'dayjs'
 
 interface ICreateCertificate {
   id: string
   name: string
   grade: string
+}
+interface ITemplate {
+  id: string
+  name: string
+  grade: string
+  medal: string
+  date: string
+}
+const compileHandlebars = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), 'src', 'templates', 'certificate.hbs')
+
+  const html = readFileSync(filePath, 'utf8')
+
+  return compile(html)(data)
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -28,6 +47,39 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     }
   }).promise()
 
+  const medalPath = join(process.cwd(), 'src', 'templates', 'selo.png')
+  const medal = readFileSync(medalPath, 'base64')
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date:dayjs().format('DD/MM/YYYY'),
+    medal
+  }
+
+
+  const content = await compileHandlebars(data)
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+    // ignoreDefaultArgs: ['--disable-extensions'],
+  })
+
+  const page = await browser.newPage()
+  await page.setContent(content)
+  const pdf = await page.pdf({
+    format: 'a4',
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? './certificate.pdf' : null
+  })
+
+  await browser.close()
   return {
     statusCode: 201,
     body: JSON.stringify(response.Items[0])
